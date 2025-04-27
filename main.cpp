512a863725b69de45c6f071cce8c0e5ff78e62ec
hotinlu06 <hotinlu06@connect.hku.hk>
2025-04-28 01:06:09 +0800

    Added hardai

--- main.cpp
+++ main.cpp
@@ -313,6 +313,48 @@ void CleanupGun() { //Free the Dynamic Array Momeries
     }
 }
 
+
+int HardAI(){
+	//this is probably too high we should change it
+    int accuracy[] = {100, 100, 100, 100, 100, 100, 100, 100, 100};
+    int random = rand() % 100;
+    int currentaccuracy = accuracy[nowSpot];
+    bool shoot;
+    if(random < currentaccuracy) {
+        shoot=isNextBullet();
+    } else {
+        shoot!=isNextBullet();
+    }
+    if(shoot){
+        if(isNextBullet()){
+            gunleft(); // gun to left, shoot player.
+            std::this_thread::sleep_for(std::chrono::milliseconds(200));
+            Damage(1,1);
+            std::this_thread::sleep_for(std::chrono::milliseconds(200));
+            return(1); //打中玩家继续回合
+        }
+        else{
+            nowSpot++;
+            std::this_thread::sleep_for(std::chrono::milliseconds(400));
+            return 0;
+        }
+    }
+    else{
+        if(isNextBullet()){
+            gunright(); // gun to right, shoot AI.
+            std::this_thread::sleep_for(std::chrono::milliseconds(200));
+            Damage(0,1);
+            std::this_thread::sleep_for(std::chrono::milliseconds(200));
+            return 0;
+        }
+        else{
+            nowSpot++;
+            std::this_thread::sleep_for(std::chrono::milliseconds(400));
+            return 1;
+        }
+    }
+}
+
 int MediumAI(){ //Game's Medium AI
     //Printtest(0);
     useHealItem(0); //Use HealItem whenever possible
