880613d9137fe16e318a47f3d4e0f013c59ce4cd
ern-lynn <u3644107@connect.hku.hk>
2025-04-29 14:50:34 +0800

    Update easyAI.cpp

--- easyAI.cpp
+++ easyAI.cpp
@@ -1,21 +1,21 @@
 int easyAI() {
 	int randomItemToUse = rand() % 4;
 	switch(randomItemToUse) { //use a random weapon
-		case 0: useNextBulletItem(0); break;
+		case 0: 
+			if (useNextBulletItem(0)) {
+				if (isNextBullet()) {
+					Damage(1, 1);
+					return 1; //shoot plater
+				}
+				else {
+					nowSpot += 1;
+					return 1;
+				}
+			}
+			break;
 		case 1: useDoubleDamItem(0); break;
 		case 2: useChainItem(0); break;
 		case 3: useHealItem(0); break;
-	}
-
-	if (useNextBulletItem(0)) {
-		if (isNextBullet()) {
-			Damage(1, 1);
-			return 1; //shoot player
-		}
-		else {
-			nowSpot += 1;
-			return 1;
-		}
 	}
 	
 	if (Probability() < 0.5) { //shoot itself
@@ -39,6 +39,7 @@ int easyAI() {
 			return 1;
 		}
 		else {
+			
 			nowSpot += 1;
 			return 0; //chance return to player
 		}
