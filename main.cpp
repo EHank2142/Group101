76524089b074080aa6b72bb261ae8f4175df125a
hotinlu06 <hotinlu06@connect.hku.hk>
2025-05-01 13:32:43 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -808,7 +808,7 @@ int easyAI() {
 // int HardAI() made by Lu Ho Tin
 
 int HardAI(){
-    int accuracy[] = {100, 95, 85, 75, 65, 60, 50, 50, 50};
+    int accuracy[] = {100, 95, 90, 85, 80, 75, 70, 65, 60};
     int random = rand() % 100;
     int currentaccuracy = accuracy[nowSpot];
     bool shoot;
